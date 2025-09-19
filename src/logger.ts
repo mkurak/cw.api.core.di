cf3a14b779b7ad8d2f47efd3eb618b0de85d9ee2
Mesut Kurak <mesutkurak@hotmail.com>
2025-09-19 23:48:32 +0000

    refactor: use shared cw logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,6 @@
-import {
-    createColoredConsole,
-    type ColoredConsole,
-    type ColoredConsoleOptions
-} from 'cw.helper.colored.console';
+import type { ColoredConsole } from 'cw.helper.colored.console';
+import { createCwLogger } from 'cw.helper.colored.console/themes/cw';
 
-const DEFAULT_LOGGER_OPTIONS: ColoredConsoleOptions = {
-    name: 'cw-di',
-    theme: {
-        info: { color: 'cyan' },
-        success: { color: 'green' },
-        warn: { color: 'yellow', bold: true },
-        error: { color: 'red', bold: true },
-        debug: { color: 'magenta', dim: true }
-    }
-};
-
-const logger: ColoredConsole = createColoredConsole(DEFAULT_LOGGER_OPTIONS);
+const logger: ColoredConsole = createCwLogger({ name: 'cw-di' });
 
 export { logger };
